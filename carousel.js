b1e4bc2acd95779591e1e39a70f8bc3fc3fffccb
ekoida <celeno4ka@gmail.com>
2024-02-07 15:49:06 +0200

    started carousel in js

---
+++ carousel.js
@@ -0,0 +1,17 @@
+// Exaples: OUL, Slick .... Bootstrap
+// 1. Function
+// 2. events
+// 3. Array, Number, String, ...
+// 4. if/else for/while
+
+const IMAGES = [
+  // hW 1- what a const
+  "img/blake-verdoorn-cssvEZacHvQ-unsplash.jpg",
+  "img/dave-hoefler-lsoogGC_5dg-unsplash.jpg0",
+  "img/goutham-krishna-h5wvMCdOV3w-unsplash.jpg",
+  "img/sapan-patel-i9Q9bc-WgfE-unsplash.jpg",
+];
+
+function showImage(n) {
+  
+}
