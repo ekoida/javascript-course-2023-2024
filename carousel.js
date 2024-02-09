ca23da6d663b49d9177040f74af8895e4e02ebd0
ekoida <celeno4ka@gmail.com>
2024-02-09 22:59:12 +0200

    added script for image slider

--- carousel.js
+++ carousel.js
@@ -7,11 +7,41 @@
 const IMAGES = [
   // hW 1- what a const
   "img/blake-verdoorn-cssvEZacHvQ-unsplash.jpg",
-  "img/dave-hoefler-lsoogGC_5dg-unsplash.jpg0",
+  "img/dave-hoefler-lsoogGC_5dg-unsplash.jpg",
   "img/goutham-krishna-h5wvMCdOV3w-unsplash.jpg",
   "img/sapan-patel-i9Q9bc-WgfE-unsplash.jpg",
 ];
+//start/current slide
+let currentIndex = 0;
 
-function showImage(n) {
-  
+function showCounterDots() {
+  counter_dots.innerHTML = "";
+  for (let i = 0; i <= IMAGES.length - 1; i++) {
+    if (i === currentIndex) {
+      counter_dots.innerHTML += `<div class="counter active"></div>`;
+    } else {
+      counter_dots.innerHTML += `<div class="counter"></div>`;
+    }
+  }
 }
+
+function showImage(direction) {
+  if (direction === "next") {
+    currentIndex++;
+
+    if (currentIndex > 3) {
+      currentIndex = 0;
+    }
+  } else if (direction === "prev") {
+    currentIndex--;
+    if (currentIndex < 0) {
+      currentIndex = 3;
+    }
+  }
+  showCounterDots();
+  carouselSlides.innerHTML = `
+  <img width=600 height=800 src="${IMAGES[currentIndex]}"/>
+  `;
+}
+
+showImage(currentIndex);
