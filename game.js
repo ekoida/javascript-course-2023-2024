88cc1d09ff185c84d98a1d1e2cad96d57f9e7e3f
ekoida <celeno4ka@gmail.com>
2024-02-09 13:08:37 +0200

    added script for boundaries,moving left and start new game

--- game.js
+++ game.js
@@ -2,6 +2,7 @@
 // 0 - empty
 // 1 - tirex
 // 2 - cactux
+let initialMap = [0, 2, 0, 0, 1, 0, 2, 0, 2, 0];
 let gameMap = [0, 2, 0, 0, 1, 0, 2, 0, 2, 0];
 let ti = 4;
 dir = "left";
@@ -9,17 +10,46 @@ dir = "left";
 function actinon() {
   switch (event.key) {
     case "ArrowLeft":
-      // HW1 -finish movement to left
-      // HW2 - boundaries - should not go out of the map
+      if (ti === 0) {
+        dir = "right";
+        drawMap();
+        break;
+      }
+      gameMap[ti] = 0;
+      ti--;
+      gameMap[ti] = 1;
+      dir = "left";
+      drawMap();
+      newGame();
       break;
     case "ArrowRight":
+      if (ti === gameMap.length - 1) {
+        dir = "left";
+        drawMap();
+        break;
+      }
       gameMap[ti] = 0;
       ti++;
       gameMap[ti] = 1;
       dir = "right";
 
       drawMap();
+      newGame();
       break;
+  }
+}
+
+function startNewGame() {
+  gameMap = initialMap;
+  ti = 4;
+
+  drawMap();
+}
+
+function newGame() {
+  if (!gameMap.includes(2)) {
+    info.innerHTML = `Game over!
+    Do you want to continue? <button onclick=startNewGame()>Continue</button>`;
   }
 }
 
