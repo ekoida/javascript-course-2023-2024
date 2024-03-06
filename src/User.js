7f07e7bbcfee86c4efac36226d19e3e3ebf81529
ekoida <celeno4ka@gmail.com>
2024-03-06 16:38:52 +0200

    added hw1

--- src/User.js
+++ src/User.js
@@ -1,16 +1,35 @@
 class User {
-  static all = []
+  static all = [];
+  static findByName(name) {
+    return User.all.find((user) => user.name === name);
+  }
 
+  static findByAgeRange(minAge, maxAge) {
+    // let usersInRange = [];
+    if (!minAge) {
+      minAge = 0;
+    }
+    if (!maxAge) {
+      maxAge = Infinity;
+    }
+    // for (let i = 0; i <= User.all.length - 1; i++) {
+    //   if (User.all[i].age >= minAge && User.all[i].age <= maxAge) {
+    //     usersInRange.push(User.all[i]);
+    //   }
+    // }
+
+    return User.all.filter((user) => user.age >= minAge && user.age <= maxAge);
+  }
   constructor(name, age, online) {
     this.name = name;
     this.age = age;
     this.online = online;
-    User.all.push(this)
+    User.all.push(this);
   }
 
   render() {
     console.log(`
-    [${this.name}] ${this.online ? "●" : "○"}
-    `);
+  [${this.name}] ${this.online ? "●" : "○"}
+  `);
   }
 }
