925026f18e84fde48ca41fe1c51bf7bbaf76efb1
ekoida <celeno4ka@gmail.com>
2024-03-21 10:17:08 +0200

    added some comments

--- main.js
+++ main.js
@@ -15,4 +15,6 @@ const toggleDropdown = (e) => {
 };
 
 let open = false;
+
+// с помощью метода querySelector находим прямых потомков(a) у класса dropdown и применям к ним метод addEventListener("определитель события "click" применённого к а и связывающегося с const toggleDropdown и запускающим  функцию с переменной (e)" )
 document.querySelector(".dropdown > a").addEventListener("click", toggleDropdown);
