c1cef0fbef39b409cd39471adadfb2505697e56f
ekoida <celeno4ka@gmail.com>
2024-03-21 10:29:31 +0200

    added some more comments

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const toggleDropdown = (e) => {
   open = !open;
   if (open) {
-    e.target
+    e.target //это свойство События (e) Event, с помощью которого Observer связывает событие click с ".dropdown > a"
     .nextElementSibling
     .style
     .display = 'block'
@@ -16,5 +16,5 @@ const toggleDropdown = (e) => {
 
 let open = false;
 
-// с помощью метода querySelector находим прямых потомков(a) у класса dropdown и применям к ним метод addEventListener("определитель события "click" применённого к а и связывающегося с const toggleDropdown и запускающим  функцию с переменной (e)" )
+// с помощью метода querySelector находим прямых потомков(a) у класса dropdown и применям к ним метод addEventListener("определитель события "click" применённого к а и связывающегося с const toggleDropdown и запускающим  функцию с параметром (e)" )
 document.querySelector(".dropdown > a").addEventListener("click", toggleDropdown);
