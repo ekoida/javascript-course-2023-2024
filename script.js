c4b239078700019274dadea550939d49989293ca
ekoida <celeno4ka@gmail.com>
2024-03-28 09:48:58 +0200

    created data object

---
+++ script.js
@@ -0,0 +1,42 @@
+const data = {
+  employees: [
+    {
+      name: "John",
+      salary: 1600,
+      age: 23,
+      avatar: "",
+      contacts: {
+        email: "john.p@gmail.com",
+        phone: "01230130123",
+        telegramm: "",
+        viber: "",
+      },
+    },
+    {
+      name: "Marry",
+      salary: 3600,
+      age: 35,
+      avatar: "",
+      contacts: {
+        email: "",
+        phone: "9837234293",
+        telegramm: "9837234293",
+        viber: "",
+      },
+    },
+    {
+      name: "Kean",
+      salary: 500,
+      age: 17,
+      avatar: "",
+      contacts: {
+        email: "kean@yahoo.com",
+        phone: "92873492384",
+        telegramm: "92873492384",
+        viber: "92873492384",
+      },
+    },
+  ],
+};
+
+
