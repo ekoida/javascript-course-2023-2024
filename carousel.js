df1eba04a485158bbbbe5818482eae3c5a9c6861
ekoida <celeno4ka@gmail.com>
2024-04-01 14:34:03 +0300

    video carousel 1 - end

--- carousel.js
+++ carousel.js
@@ -1,27 +1,34 @@
-const renderSlide = () => {
-  let parentDiv = document.querySelector(".carousel");
-
-  let slide = document.createElement("div");
-  slide.className = "slide";
-
-  for (let y = 1; y <= 12; y++) {
-    for (let x = 1; x <= 16; x++) {
-      let sq = document.createElement("div");
-      sq.className = "sq";
-      // разобраться в том как размещаются координаты
-      sq.style.top = `${(y - 1) * 50}px`;
-      sq.style.left = `${(x - 1) * 50}px`;
-
-      sq.style.backgroundImage = `url(image/1-img.jpg)`;
-
-      sq.style.backgroundPositionY = `${-(y - 1) * 50}px`;
-      sq.style.backgroundPositionX = `${-(x - 1) * 50}px`;
-
-      slide.appendChild(sq);
-    }
+class Slide {
+  constructor(url) {
+    this.url = url;
   }
 
-  parentDiv.appendChild(slide);
-};
+  render(rootSelector) {
+    let parentDiv = document.querySelector(rootSelector);
 
-renderSlide();
+    let slide = document.createElement("div");
+    slide.className = "slide";
+
+    for (let y = 1; y <= 12; y++) {
+      for (let x = 1; x <= 16; x++) {
+        let sq = document.createElement("div");
+        sq.className = "sq";
+        // разобраться в том как размещаются координаты
+        sq.style.top = `${(y - 1) * 50}px`;
+        sq.style.left = `${(x - 1) * 50}px`;
+
+        sq.style.backgroundImage = `url(${this.url})`;
+
+        sq.style.backgroundPositionY = `${-(y - 1) * 50}px`;
+        sq.style.backgroundPositionX = `${-(x - 1) * 50}px`;
+
+        slide.appendChild(sq);
+      }
+    }
+
+    parentDiv.appendChild(slide);
+  }
+}
+
+const slides = [new Slide("image/1-img.jpg"), new Slide("image/2-img.jpg"), new Slide("image/3-img.jpg")];
+slides[0].render(".carousel");
