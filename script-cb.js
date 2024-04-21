c498f3253352797db5eaeb2f7e9abc4fdd2131f1
ekoida <celeno4ka@gmail.com>
2024-04-21 13:22:55 +0300

    added HW1another way to get max value

--- script-cb.js
+++ script-cb.js
@@ -1,14 +1,16 @@
 // I/O input - output
 
 const getNumbers = (cb, size = 10) => {
+  //2.
   let numbers = [];
-  setTimeout(() => {
+  setTimeout(function firstTimeOut() {
+    //3.
     // HW2 set interval ()
     while (numbers.length < size) {
       numbers.push(Math.ceil(-5 + Math.random() * 10));
     }
 
-    cb(numbers);
+    cb(numbers); //4.
   }, Math.random() * 3000);
 
   return numbers;
@@ -16,18 +18,25 @@ const getNumbers = (cb, size = 10) => {
 
 // prpocess - async
 const maxNumber = (cb, numbers) => {
+  //7.
   // HW1 - think another way to get max value
 
-  let maxNumber = numbers[0];
+  //let maxNumber = numbers[0];
 
-  setTimeout(() => {
-    for (let i = 0; i < numbers.length; i++) {
-      if (numbers[i] > maxNumber) {
-        maxNumber = numbers[i];
-      }
-    }
+  setTimeout(function secondTimeOut() {
+    //8.
 
-    cb(maxNumber);
+    //FIRST WAY TO GET MAX VALUE
+    // for (let i = 0; i < numbers.length; i++) {
+    //   if (numbers[i] > maxNumber) {
+    //     maxNumber = numbers[i];
+    //   }
+    // }
+    //SECOND WAY TO GET MAX VALUE
+    let maxNumber = numbers.toSorted((a, b) => b - a)[0];
+    //THIRD WAY TO GET MAX VALUE
+    //let maxNumber = Math.max(...numbers)
+    cb(maxNumber); //9.
   }, Math.random() * 3000);
 };
 
@@ -36,6 +45,8 @@ const renderNumber = (number) => {
   console.log(">>>>>", number);
 };
 
-getNumbers((numbers) => {
-  maxNumber(renderNumber, numbers);
+getNumbers(function firstCallback(numbers) {
+  //1.
+  //5.
+  maxNumber(renderNumber, numbers); //6.
 });
