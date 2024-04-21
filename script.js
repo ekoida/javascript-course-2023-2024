4b1f437d3ba07792ee18b0802b55fba67cc6c8d7
ekoida <celeno4ka@gmail.com>
2024-04-21 18:45:20 +0300

    added reminder for tomorrow and upcoming week

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const calendarHoliday = document.querySelector("#calendarHolydays");
-
+const reminderContainer = document.querySelector(".reminder");
 // 1
 navigator.geolocation.getCurrentPosition(async (position) => {
   const lat = position.coords.latitude;
@@ -19,9 +19,16 @@ navigator.geolocation.getCurrentPosition(async (position) => {
   let countryCode = response.countryCode;
 
   // 2
+  const numberOfDays = 7;
   let date = new Date();
-
-  date.setDate(date.getDate() + 12); // 2024-05-1
+  let period = [];
+  for (i = 1; i <= numberOfDays; i++) {
+    let calculatedDate = new Date("2024-04-30");
+    const dayInTheFuture = new Date(calculatedDate.setDate(calculatedDate.getDate() + i)).toISOString().split("T")[0];
+    period.push(dayInTheFuture);
+  }
+  console.log(period);
+  date.setDate(date.getDate() + 9);
 
   let year = date.getFullYear();
   const dateISO = date.toISOString().split("T")[0];
@@ -30,6 +37,33 @@ navigator.geolocation.getCurrentPosition(async (position) => {
   const holydays = await calendar.json();
 
   let holiday = holydays.find((day) => day.date === dateISO);
+
+  for (i = 0; i < holydays.length; i++) {
+    const day = document.createElement("div");
+    const dataHolyday = document.createElement("div");
+    const nameHolyday = document.createElement("div");
+    if (dateISO === holydays[i].date) {
+      day.classList.add("celebrateDay");
+    }
+
+    period.forEach((day, index) => {
+      if (index == 0 && day === holydays[i].date) {
+        reminderContainer.innerText = `Tomorrow is ${holydays[i].name}\n`;
+      }
+      if (index !== 0 && day === holydays[i].date) {
+        reminderContainer.innerText += `Soon is ${holydays[i].name}\n`;
+      }
+    });
+    day.classList.add("dayHolyday");
+
+    dataHolyday.innerText = holydays[i].date;
+    nameHolyday.innerText = holydays[i].name;
+
+    day.append(dataHolyday);
+    day.append(nameHolyday);
+
+    calendarHoliday.append(day);
+  }
 
   if (holiday) {
     let h3 = document.createElement("h3");
