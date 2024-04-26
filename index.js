7e2053e719ac650c5c4b2b4e10cc8a50567d1462
ekoida <celeno4ka@gmail.com>
2024-04-26 15:29:32 +0300

    hw1 - format rating to have one integer after dot

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ const news = [
 ]
 
 
-const ratingMap = [`☆ ☆ ☆ ☆ ☆`, `★ ☆ ☆ ☆ ☆`, `★ ★ ★ ☆ ☆`, `★ ★ ★ ★ ☆`, `★ ★ ★ ★ ★`];
+const ratingMap = [
+  `☆ ☆ ☆ ☆ ☆`,
+  `★ ☆ ☆ ☆ ☆`,
+  `★ ★ ☆ ☆ ☆`,
+  `★ ★ ★ ☆ ☆`,
+  `★ ★ ★ ★ ☆`,
+  `★ ★ ★ ★ ★`,
+];
 
 console.clear()
 console.log("NEWS\n\n")
 
-// for (let i = 0; i < news.length; i++) {
+for (let i = 0; i < news.length; i++) {
 
-//     // HW1 - format rating ---->5.0
-//     let template = ` ${news[i].title} (${news[i].rating})\n`+
-//     `\t - ${news[i].publishedOn} \n`+
-//     `<${news[i].author}> \n`
+    // HW1 - format rating ---->5.0
+    let template = ` ${news[i].title} (${news[i].rating.toFixed(1)})\n`+
+    `\t - ${news[i].publishedOn} \n`+
+    `<${news[i].author}> \n`
 
-//     console.log(template)
-// }
-news.forEach((item) => {
-    let rating = Math.round(item.rating.toFixed(1))
-    let template = `${item.title} (${ratingMap[rating - 1]})\n`+
-    `\t - ${item.publishedOn}` +
-    `<${item.author}>\n`
     console.log(template)
-})
+}
