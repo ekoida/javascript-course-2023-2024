d6a15027febab7a480a302d66e6adc63a1638041
ekoida <celeno4ka@gmail.com>
2024-04-30 22:40:21 +0300

    wrote file system lesson

--- index.mjs
+++ index.mjs
@@ -1,5 +1,18 @@
-import news from "./data.mjs"
-import renderNews from "./ui.mjs"
+import { readFile, writeFile } from "node:fs/promises";
+
+let data = await readFile("./news.json");
+
+data = JSON.parse(data);
+
+data.forEach((newsItem) => {
+  console.log(`${newsItem.title} <${newsItem.author}>`);
+});
+
+let title = "This is a sample text";
+await writeFile("./news.txt", title);
+
+let news = [{ title: "First Title" }, { title: "Second Title" }];
+await writeFile("./news-written.json", JSON.stringify(news))
 
 
-renderNews(news)
+// HW1: create a diagram for write and for write an json
