e718731e4f22ca7998d2d04cce1fdf6b137366f5
ekoida <celeno4ka@gmail.com>
2024-05-09 17:37:59 +0300

    added rating flexible calculation using FOR, added precision

--- index.mjs
+++ index.mjs
@@ -10,14 +10,19 @@ const io = readline.createInterface({
 });
 
 io.question("enter 3 rating values separated by space: ", (answer) => {
+ 
   // HW0*: review .forEach .filter() split() .map()
   let ratings = answer.split(" ").map((value) => parseFloat(value));
 
   console.log(ratings);
 
-  // HW2: calculate this using standard for()
+  // HW2: calculate sum using standard for()
+  let sumRatings = 0;
+  for (let i = 0; i < ratings.length; i++) {
+    sumRatings += ratings[i];
+  }
 
-  // HW3: calculate this using Array.reduce()
+  // HW3: calculate sum using Array.reduce()
 
   // HW4: make the calkulator flexible(accept form 2 ... 10 ratings)
 
@@ -26,9 +31,11 @@ io.question("enter 3 rating values separated by space: ", (answer) => {
 
   // HW6*: in a separate branch create app, that load the rating from the json file
   // calculates the average and outputs it into console
-  let avgRating = (ratings[0] + ratings[1] + ratings[2]) / 3;
+
+  let avgRating = (sumRatings / ratings.length).toFixed(1);
 
   // HW1: format the output with only 1 digit precision 4.6 \ 4.7
+
   console.log(`the average reating is : ${avgRating}`);
   io.close();
 });
