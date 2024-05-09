9a3d2adac7832f33b6e116a3e8607bae4621add7
ekoida <celeno4ka@gmail.com>
2024-05-09 18:30:28 +0300

    added readFile from the input calculates and save it in s json file

--- index.mjs
+++ index.mjs
@@ -3,6 +3,7 @@
 // outputs the avg
 
 import readline from "node:readline";
+import { writeFile, readFile } from "node:fs/promises";
 
 const io = readline.createInterface({
   input: process.stdin,
@@ -21,7 +22,10 @@ io.question("enter 3 rating values separated by space: ", (answer) => {
   //sumRatings += ratings[i];
   //}
   const initialValue = 0;
-  const sumRatings = ratings.reduce((accumulator, rating) => accumulator + rating, initialValue);
+  const sumRatings = ratings.reduce(
+    (accumulator, rating) => accumulator + rating,
+    initialValue
+  );
 
   // HW3: calculate sum using Array.reduce()
 
@@ -29,14 +33,18 @@ io.question("enter 3 rating values separated by space: ", (answer) => {
 
   // HW5*: in a separate branch create app, that load the rating from the input
   // calculates the average and saves it in a json file back
+  let avgRating = (sumRatings / ratings.length).toFixed(1);
 
+  readFile("./data/news.json").then((content) => {
+    const news = JSON.parse(content);
+    news[0].rating = avgRating;
+
+    writeFile("./data/news.json", JSON.stringify(news, null, 2));
+  });
   // HW6*: in a separate branch create app, that load the rating from the json file
   // calculates the average and outputs it into console
 
-  let avgRating = (sumRatings / ratings.length).toFixed(1);
-
   // HW1: format the output with only 1 digit precision 4.6 \ 4.7
-
   console.log(`the average reating is : ${avgRating}`);
   io.close();
 });
