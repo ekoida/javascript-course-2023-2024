57dd9cab4ebfc48fabb1840f1c002e2820f21b8e
ekoida <celeno4ka@gmail.com>
2024-05-24 19:45:29 +0300

    prepared directories for work

--- User.mjs
+++
@@ -1,9 +0,0 @@
-class User {
-  constructor(name, email, password) {
-      this.name = name;
-      this.password = password;
-      this.email = email;
-    }
-}
-
-export default User;
--- index.mjs
+++
@@ -1,7 +0,0 @@
-import getAllNews from "./storage.mjs"
-import renderNews from "./ui.mjs"
-
-
-let news = await getAllNews()
-
-renderNews(news)
