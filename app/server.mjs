57304daca6cb7a3745c54d906e2713b9bffeb7f6
ekoida <celeno4ka@gmail.com>
2024-05-28 20:54:08 +0300

    conspected homework

--- app/server.mjs
+++ app/server.mjs
@@ -1,47 +1,66 @@
 import http from "node:http";
 import fs from "node:fs";
 
-// HW:1 modify the html file
-// and the server logic
-// so you load a favicon / logo into the page
+const routes = {
+  "/": "index.html",
+  "/style.css": "css/style.css",
+  "/favicon.ico": "favicon.ico",
+  "/product.jpg": "img/product.jpg",
+};
+
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    fs.readFile("index.html", (err, data) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-
-      res.write(data);
-
-      res.end();
-    });
-  } else if (req.url === "/style.css") {
-    fs.readFile("style.css", (err, data) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-
-      res.write(data);
-
-      res.end();
-    });
-  } else if (req.url === "/img/favicon.ico") {
-    fs.readFile("img/favicon.ico", (err, data) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-
-      res.write(data);
-
-      res.end();
-    });
-  } else {
+  if (!routes[req.url]) {
     res.write("Not found");
     res.end();
+
+    return;
   }
+
+  fs.readFile(routes[req.url], (err, data) => {
+    if (err) {
+      console.log(err);
+      res.write(err.message);
+      res.end();
+      return;
+    }
+
+    // 1- react only for html page
+    if (routes[req.url] === "index.html") {
+      // 2 - load json
+      fs.readFile("data/product.json", (err, dataJSON) => {
+        if (err) {
+          console.log(err);
+          res.write(err.message);
+          res.end();
+          return;
+        }
+
+        const {
+          title,
+          subtitle,
+          description,
+          image,
+          price: { amount, currency },
+        } = JSON.parse(dataJSON);
+
+        data = data
+          .toString()
+          .replace("{title}", title)
+          .replace("{subtitle}", subtitle)
+          .replace("{description}", description)
+          .replace("{image}", image)
+          .replace("{priceAmount}", amount)
+          .replace("{priceCurrency}", currency);
+
+        res.write(data);
+
+        res.end();
+      });
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 
   console.log("a requset inserted");
 });
