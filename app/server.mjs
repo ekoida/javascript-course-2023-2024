0a470dd11d8ede64642318332b565cebc3117f87
ekoida <celeno4ka@gmail.com>
2024-05-29 11:49:58 +0300

    added tags

--- app/server.mjs
+++ app/server.mjs
@@ -1,6 +1,10 @@
 import http from "node:http";
 import fs from "node:fs";
 
+// HW2 - imagine that "tags": ["JS", "learning", "education", "2018", "programming", "IT"]
+// is added to your JSON file data.
+// - refactor the html template
+// - refator the server script using a loop so teh tags would be rendered as an unordered list
 const routes = {
   "/": "index.html",
   "/style.css": "css/style.css",
@@ -24,9 +28,7 @@ const server = http.createServer((req, res) => {
       return;
     }
 
-    // 1- react only for html page
     if (routes[req.url] === "index.html") {
-      // 2 - load json
       fs.readFile("data/product.json", (err, dataJSON) => {
         if (err) {
           console.log(err);
@@ -40,8 +42,15 @@ const server = http.createServer((req, res) => {
           subtitle,
           description,
           image,
+          tags,
           price: { amount, currency },
         } = JSON.parse(dataJSON);
+
+        const tagsString = tags
+          .map((tag) => {
+            return `<li>${tag}</li>`;
+          })
+          .join("");
 
         data = data
           .toString()
@@ -49,6 +58,7 @@ const server = http.createServer((req, res) => {
           .replace("{subtitle}", subtitle)
           .replace("{description}", description)
           .replace("{image}", image)
+          .replace("{tags}",tagsString)
           .replace("{priceAmount}", amount)
           .replace("{priceCurrency}", currency);
 
