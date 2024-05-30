5d411526b8f1e0b2da6492873c4d822de7b10bb5
ekoida <celeno4ka@gmail.com>
2024-05-30 09:54:24 +0300

    transscripted lesson for client rendering

--- app/server.mjs
+++ app/server.mjs
@@ -1,10 +1,6 @@
 import http from "node:http";
 import fs from "node:fs";
 
-// HW2 - imagine that "tags": ["JS", "learning", "education", "2018", "programming", "IT"]
-// is added to your JSON file data.
-// - refactor the html template
-// - refator the server script using a loop so teh tags would be rendered as an unordered list
 const routes = {
   "/": "index.html",
   "/style.css": "css/style.css",
@@ -13,66 +9,35 @@ const routes = {
 };
 
 const server = http.createServer((req, res) => {
-  if (!routes[req.url]) {
-    res.write("Not found");
-    res.end();
-
-    return;
-  }
-
-  fs.readFile(routes[req.url], (err, data) => {
-    if (err) {
-      console.log(err);
-      res.write(err.message);
-      res.end();
-      return;
-    }
-
-    if (routes[req.url] === "index.html") {
-      fs.readFile("data/product.json", (err, dataJSON) => {
-        if (err) {
-          console.log(err);
-          res.write(err.message);
-          res.end();
-          return;
-        }
-
-        const {
-          title,
-          subtitle,
-          description,
-          image,
-          tags,
-          price: { amount, currency },
-        } = JSON.parse(dataJSON);
-
-        const tagsString = tags
-          .map((tag) => {
-            return `<li>${tag}</li>`;
-          })
-          .join("");
-
-        data = data
-          .toString()
-          .replace("{title}", title)
-          .replace("{subtitle}", subtitle)
-          .replace("{description}", description)
-          .replace("{image}", image)
-          .replace("{tags}",tagsString)
-          .replace("{priceAmount}", amount)
-          .replace("{priceCurrency}", currency);
-
-        res.write(data);
-
+  if (routes[req.url]) {
+    fs.readFile(routes[req.url], (err, data) => {
+      if (err) {
+        console.log(err);
+        res.write("Error is happend");
         res.end();
-      });
-    } else {
+        return;
+      }
+
       res.write(data);
       res.end();
-    }
-  });
+    });
+  } else if (req.url === "/api/product") {
+    fs.readFile("data/product.json", (err, data) => {
+      if (err) {
+        console.log(err);
+        res.write("Can't read the json file");
+        res.end();
+        return;
+      }
 
-  console.log("a requset inserted");
+      res.write(data);
+      res.end();
+    });
+  } else {
+    res.write("Not found");
+    res.end();
+    return;
+  }
 });
 
 server.listen(8889);
